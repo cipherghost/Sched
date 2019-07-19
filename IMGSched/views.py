03e031e6d34afa8223554c031e3c77350e0b246a
cipherghost <spranav13579@gmail.com>
2019-07-20 00:24:18 +0530

    by parkhi part 2

--- IMGSched/views.py
+++ IMGSched/views.py
@@ -1,31 +1,28 @@
-# IMGSched/views.py
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.utils.decorators import method_decorator
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.utils import timezone
 from django.shortcuts import get_object_or_404
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from IMGSched.serializers import UserSerializer, CommentSerializer, MeetingSerializer
+from IMGSched.models import Meeting, Comment
+# from IMGSched.permissions import IsOwnerOrReadOnly, IsOwnerOrRead
 
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework import permissions, generics, viewsets
-
-from IMGSched.serializers import UserSerializer, CommentSerializer, MeetingSerializer
-from IMGSched.models import Meeting, Comment
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
+# Create your views here.
 class MeetingViewSet(viewsets.ModelViewSet):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
     permission_classes = (IsAuthenticated, )
 
+
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
-        serializer.save(created_on=timezone.now())    
+        serializer.save(created_on=timezone.now()) 
 
     @action(detail=True, methods=['get'])
     def get_queryset(self):
@@ -37,17 +34,17 @@ class MeetingViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    #permission_classes = (IsAuthenticated, )
 
-    def perform_create(self, serializer):
-        # serializer.save(user=self.request.user)  
+    def perform_create(self, serializer): 
         serializer.save(time=timezone.now())
 
-    # def retrieve(self, request, pk=None) :
-    #     quesryset = Comment.objects.filter(meeting_id = pk)
-    #     serializer = CommentSerializer(queryset, many = True)
-    #     return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        queryset = Comment.objects.filter(meeting_id=pk)
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data)    
 
-class UserViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated, )
@@ -59,16 +56,18 @@ class UserViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
         print(queryset)
         return Response(serializer.data)
 
+
     def get_object(self):
         queryset = self.get_queryset()
         queryset = self.filter_queryset(queryset)
         filter = {}
         print(self.kwargs['pk'].isnumeric())
         if self.kwargs['pk'].isnumeric():
-            filter['pk']=self.kwargs['pk']
+            filter['pk'] = self.kwargs['pk']
         else:
-            filter['username']=self.kwargs['pk']
-        return get_object_or_404(queryset, **filter)            
+            filter['username'] = self.kwargs['pk']
+        return get_object_or_404(queryset, **filter)
+
 
     def get_permissions(self):
         if self.action == 'create':
